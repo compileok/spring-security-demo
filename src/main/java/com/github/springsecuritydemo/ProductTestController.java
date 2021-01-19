acb28708f14a743a5aed252cdcc04c1acebd38ee
rusong <rusong@helijia.com>
2021-01-20 01:04:36 +0800

    补充内容

--- src/main/java/com/github/springsecuritydemo/ProductTestController.java
+++ src/main/java/com/github/springsecuritydemo/ProductTestController.java
@@ -1,5 +1,7 @@
 package com.github.springsecuritydemo;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,14 @@ public class ProductTestController {
 
     @RequestMapping("/info")
     public String productInfo(){
-        return " some product info ";
+        String currentUser = "";
+        Object principl = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if(principl instanceof UserDetails) {
+            currentUser = ((UserDetails)principl).getUsername();
+        }else {
+            currentUser = principl.toString();
+        }
+        return " some product info,currentUser is: "+currentUser;
     }
 
 }
--- src/main/java/com/github/springsecuritydemo/config/SecurityConfiguration.java
+++ src/main/java/com/github/springsecuritydemo/config/SecurityConfiguration.java
@@ -1,14 +1,16 @@
 package com.github.springsecuritydemo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,25 +18,48 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                    .antMatchers("/product/**").hasRole("USER")
+//                    .antMatchers("/admin/**").hasRole("ADMIN")
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin().and()
+//                .httpBasic();
+
         http
                 .authorizeRequests()
-                    .antMatchers("/product/**").hasRole("USER")
-                    .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/product/**").hasAuthority("USER")
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().and()
                 .httpBasic();
-    }
+     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                    .withUser("admin1").password("{noop}admin1").roles("ADMIN", "USER") // 管理员，同事具有 ADMIN,USER权限，可以访问所有资源
-                .and()
-                    .withUser("user1").password("{noop}user1").roles("USER"); // 普通用户，只能访问 /product/**
+        // in memory test
+//        auth
+//            .inMemoryAuthentication()
+//            .withUser("admin").password("{noop}adminpass").roles("ADMIN", "USER") // 管理员，同事具有 ADMIN,USER权限，可以访问所有资源
+//            .and()
+//            .withUser("spring").password("{noop}123456").roles("USER"); // 普通用户，只能访问有限的资源。
+
+
+         //db user test
+        auth.userDetailsService(userDetailsService)// 设置自定义的userDetailsService
+                .passwordEncoder(passwordEncoder());
+
+     }
+
+     @Autowired
+     private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //为了演示方便，我们使用NoOpPasswordEncoder（这个就是不加密）
+        return NoOpPasswordEncoder.getInstance();
     }
 
-    // Deprecated configuration property 'security.user.password'
-    // java.lang.IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
 }
